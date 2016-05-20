8c0e8c669cd361b1033065c9bca62c36d0dbf6ab
steelbrain <de.steelbrain@gmail.com>
2016-05-19 21:53:21 -0700

    :new: Initial commit

---
+++ index.js
@@ -0,0 +1,49 @@
+'use strict'
+
+function promisedFit(name, callback) {
+  fit(name, function() {
+    const value = callback()
+    if (value && value.constructor.name === 'Promise') {
+      waitsForPromise({ timeout: 10 * 1000 }, function() {
+        return value
+      })
+    }
+  })
+}
+
+function promisedIt(name, callback) {
+  it(name, function() {
+    const value = callback()
+    if (value && value.constructor.name === 'Promise') {
+      waitsForPromise({ timeout: 10 * 1000 }, function() {
+        return value
+      })
+    }
+  })
+}
+
+function promisedWait(timeout) {
+  return new Promise(function(resolve) {
+    setTimeout(resolve, timeout)
+  })
+}
+
+// Jasmine 1.3.x has no sane way of resetting to native clocks, and since we're
+// gonna test promises and such, we're gonna need it
+function resetClock() {
+  for (const key in jasmine.Clock.real) {
+    if (jasmine.Clock.real.hasOwnProperty(key)) {
+      window[key] = jasmine.Clock.real[key]
+    }
+  }
+}
+
+beforeEach(function() {
+  resetClock()
+})
+
+module.exports = {
+  it: promisedIt,
+  fit: promisedFit,
+  wait: promisedWait,
+}
